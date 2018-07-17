bded127aff2900208142bd64f2f4c96b4a6afc7e
Simon Knott <simoknott@gmail.com>
2018-07-17 14:45:50 +0200

    email works

--- src/RailMail/Dispatcher.fs
+++ src/RailMail/Dispatcher.fs
@@ -6,17 +6,18 @@ open System.Net
 
 open RailMail.Envelope
 
-let SMTP_HOST = Environment.GetEnvironmentVariable("SMTP_HOST")
-let SMTP_PORT = Environment.GetEnvironmentVariable("SMTP_PORT") |> int
-let SMTP_USERNAME = Environment.GetEnvironmentVariable("SMTP_USERNAME")
-let SMTP_PASSWORD = Environment.GetEnvironmentVariable("SMTP_PASSWORD")
-let SMTP_SENDER = Environment.GetEnvironmentVariable("SMTP_SENDER")
+let SMTP_HOST = Environment.GetEnvironmentVariable "SMTP_HOST"
+let SMTP_PORT = Environment.GetEnvironmentVariable "SMTP_PORT" |> int
+let SMTP_USERNAME = Environment.GetEnvironmentVariable "SMTP_USERNAME"
+let SMTP_PASSWORD = Environment.GetEnvironmentVariable "SMTP_PASSWORD"
+let SMTP_SENDER = Environment.GetEnvironmentVariable "SMTP_SENDER"
 
 let client = new SmtpClient(SMTP_HOST, SMTP_PORT)
+client.EnableSsl <- true
 client.Credentials <- NetworkCredential(SMTP_USERNAME, SMTP_PASSWORD)
 client.DeliveryMethod <- SmtpDeliveryMethod.Network
 
-let private constructMessage (e: Envelope) =
+let private constructMessage (e : Envelope) =
   new MailMessage(
     SMTP_SENDER,
     e.recipient,
@@ -24,6 +25,6 @@ let private constructMessage (e: Envelope) =
     e.body.text
   )
 
-let dispatch (e: Envelope) =
+let dispatch (e : Envelope) =
   use msg = constructMessage e
   client.Send msg
